0d1789e54385d42299f5900050c877bfe2a003e2
Juan-SEO <98541382+Juan-SEO@users.noreply.github.com>
2024-02-06 17:05:48 +0100

    Subida de la web JUANTECH

---
+++ script.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const carrito = document.querySelector('.carrito');
+    const contadorCarrito = document.querySelector('.contador-carrito');
+    const btnsAnadirCarrito = document.querySelectorAll('.btn-anadir-carrito');
+    const botonesAnadirCarrito = document.querySelectorAll('.btn-anadir-carrito');
+
+    let carritoCount = parseInt(localStorage.getItem('carritoCount')) || 0;
+    actualizarContadorCarrito(); 
+
+    btnsAnadirCarrito.forEach(function (btn) {
+        btn.addEventListener('click', function () {
+            carritoCount++;
+            actualizarContadorCarrito();
+
+            var producto = this.getAttribute('data-producto');
+            alert('Producto añadido al carrito: ' + producto);
+
+            
+            let productosSeleccionados = JSON.parse(localStorage.getItem('productosSeleccionados')) || [];
+            productosSeleccionados.push(producto);
+            localStorage.setItem('productosSeleccionados', JSON.stringify(productosSeleccionados));
+        });
+    });
+
+    function actualizarContadorCarrito() {
+        contadorCarrito.textContent = carritoCount;
+        localStorage.setItem('carritoCount', carritoCount);
+    }
+
+
+
+carrito.addEventListener('click', function () {
+        window.location.href = '/carrito.html';
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    mostrarProductosSeleccionados();
+});
+
+function mostrarProductosSeleccionados() {
+    const listaProductos = JSON.parse(localStorage.getItem('productosSeleccionados')) || [];
+
+    const listaProductosDiv = document.getElementById('lista-productos');
+
+    if (listaProductos.length === 0) {
+        listaProductosDiv.innerHTML = '<p>No hay productos en el carrito.</p>';
+    } else {
+        listaProductosDiv.innerHTML = '<ul>';
+        listaProductos.forEach(function (producto) {
+            listaProductosDiv.innerHTML += '<li>' + producto + '</li>';
+        });
+        listaProductosDiv.innerHTML += '</ul>';
+    }
+}
+
+
+function comprarAhora() {
+
+    window.location.href = "gracias.html";
+}
+
+// VACIAR CARRITO DE COMPRA
+
+function vaciarCarrito() {
+    localStorage.removeItem('productosSeleccionados');
+    actualizarListaProductos(); 
+}
+
+// AACTUALIZARR PRODUCTOS
+
+function actualizarListaProductos() {
+    const listaProductosDiv = document.getElementById('lista-productos');
+    listaProductosDiv.innerHTML = '<p>No hay productos en el carrito.</p>';
+}
